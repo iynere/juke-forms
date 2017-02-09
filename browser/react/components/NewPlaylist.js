3e9efb0e69765909aed15992993903adf4d0a718
mlkiung <meredith.kiung@gmail.com>
2017-02-09 16:03:19 -0500

    working on playlists

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -1,31 +1,75 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router';
 
-const NewPlaylist = props => {
-	return (
-		<div className="well">
-		  <form className="form-horizontal" onSubmit={props.onSubmit}>
-		    <fieldset>
-		      <legend>New Playlist</legend>
-		      <div className="form-group">
-		        <label className="col-xs-2 control-label">Name</label>
-		        <div className="col-xs-10">
-		          <input
-		          	className="form-control"
-		          	placeholder="Enter playlist name"
-		          	type="text"
-		          	onChange={props.handleChange}
-		          />
-		        </div>
-		      </div>
-		      <div className="form-group">
-		        <div className="col-xs-10 col-xs-offset-2">
-		          <button type="submit" className="btn btn-success">Create Playlist</button>
-		        </div>
-		      </div>
-		    </fieldset>
-		  </form>
-		</div>
-	)
+class NewPlaylist extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			inputValue: '',
+			disabled: true
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		const input = event.target.value;
+		this.setState({
+			inputValue: input.length < 17 ? input : input.slice(0, 16),
+			disabled: !input.length
+		});
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+		console.log(event.target.value);
+		axios.post(`/api/playlists`, {
+			name: event.target.value
+		})
+			.then(res => res.data)
+			.then(result => {
+				console.log(result) // response json from the server!
+			})
+			.then(() => {
+				event.target.value = '';
+				this.setState({
+					inputValue: event.target.value,
+					disabled: true
+				});
+			});
+	}
+
+
+	render() {
+		return (
+			<div className="well">
+				<form className="form-horizontal" onSubmit={this.onSubmit}>
+					<fieldset>
+						<legend>New Playlist</legend>
+						<div className="form-group">
+							<label className="col-xs-2 control-label">Name</label>
+							<div className="col-xs-10">
+								<input
+									className="form-control"
+									placeholder="Enter playlist name"
+									type="text"
+									onChange={this.handleChange}
+									value={this.state.inputValue}
+								/>
+							</div>
+						</div>
+						<div className="form-group">
+							<div className="col-xs-10 col-xs-offset-2">
+									<button type="submit" className="btn btn-success" disabled={this.state.disabled} >Create Playlist</button>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default NewPlaylist;
--- browser/react/containers/NewPlaylistContainer.js
+++ browser/react/containers/NewPlaylistContainer.js
@@ -1,36 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import NewPlaylist from '../components/NewPlaylist';
+import axios from 'axios';
 
-export default class NewPlaylistContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			inputValue: ''
-		};
-		
-		this.handleChange = this.handleChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
-	
-	handleChange(event) {
-		event.preventDefault();
-		console.log(event.target.value);
-		this.setState({inputValue: event.target.value});
-	}
-	
-	onSubmit(event) {
-		event.preventDefault();
-		console.log(this.state.inputValue);
-	}
-	
-	render() {	
-		return (
-			<div>
-				<NewPlaylist
-					handleChange={this.handleChange}
-					onSubmit={this.onSubmit}
-				/>
-			</div>
-			);
-	}
-}
+const NewPlaylistContainer = () => {
+
+
+
+	// componentDidMount() {
+	// 	axios.post(`/api/playlists`, { /** req.body contents go here! */ })
+	// 		.then(res => res.data)
+	// 		.then(result => {
+	// 			console.log(result) // response json from the server!
+	// 	});
+	// }
+
+
+	return (
+		<div>
+			<NewPlaylist />
+		</div>
+	);
+
+};
+
+export default NewPlaylistContainer;
